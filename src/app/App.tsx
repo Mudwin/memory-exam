93c9ce3eebc468da20031c5419d70fe629aaca34
Mudwin <nshmilsson@gmail.com>
2026-07-01 21:58:52 +0500

    feat: create TestButton component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import testPreview1 from "@/assets/images/44.jpg";
 import testPreview2 from "@/assets/images/48.jpg";
 import testPreview3 from "@/assets/images/50.jpg";
 import ImageCard from "@/components/ImageCard";
+import TestButton from "@/components/TestButton";
 
 function App() {
   return (
@@ -21,6 +22,11 @@ function App() {
         title="Царевна-Лебедь"
         author="Михаил Врубель"
       />
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <TestButton answer="Тестовый ответ" />
+        <TestButton answer="Тестовый ответ" answerStatus="correct" />
+        <TestButton answer="Тестовый ответ" answerStatus="error" />
+      </div>
     </div>
   );
 }
