61828a245f02cc5aa92ba37bd6c7d2a202050bf0
Mudwin <nshmilsson@gmail.com>
2026-07-01 20:24:03 +0500

    feat: create ImageCard component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import testPreview1 from "@/assets/images/44.jpg";
+import testPreview2 from "@/assets/images/48.jpg";
+import testPreview3 from "@/assets/images/50.jpg";
+import ImageCard from "@/components/ImageCard";
 
 function App() {
-  return <div>some ui component</div>;
+  return (
+    <div className="app-container">
+      <ImageCard
+        srcUrl={testPreview1}
+        title="Апофеоз войны"
+        author="Василий Верещагин"
+      />
+      <ImageCard
+        srcUrl={testPreview2}
+        title="После побоища Игоря Святославича с половцами"
+        author="Виктор Васнецов"
+      />
+      <ImageCard
+        srcUrl={testPreview3}
+        title="Царевна-Лебедь"
+        author="Михаил Врубель"
+      />
+    </div>
+  );
 }
 
 export default App;
