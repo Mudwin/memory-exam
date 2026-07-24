7c9a6e94f6d8fe52eb5295d694d8a60f1b0b62e9
Mudwin <nshmilsson@gmail.com>
2026-07-24 10:08:27 +0500

    docs: update README.md

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/memory-exam/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/memory-exam/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
